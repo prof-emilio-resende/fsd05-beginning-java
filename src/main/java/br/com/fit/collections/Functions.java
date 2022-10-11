76810b4a3b0e7f085fb749251cce8ba0a57061ca
Emilio Resende <emilio.resende@faculdadeimpacta.com.br>
2022-10-10 22:14:55 -0300

    functional

--- src/main/java/br/com/fit/collections/Functions.java
+++ src/main/java/br/com/fit/collections/Functions.java
@@ -1,9 +1,39 @@
 package br.com.fit.collections;
 
+import java.util.function.Function;
+
 public class Functions {
 
+    public static final String separator = "-->";
+    private static final String defaultPrefix = "[prefix]";
+
+    public static Function<String, String> loggerBuilder() {
+        return loggerBuilder(defaultPrefix);
+    }
+
+    public static Function<String, String> loggerBuilder(String prefix) {
+        return loggerBuilder(prefix, separator);
+    }
+
+
+    public static Function<String, String> loggerBuilder(String prefix, String sep) {
+        return str -> {
+            String txtFinal = String.format("%s %s %s", prefix, sep, str);
+            System.out.println(txtFinal);
+          
+            return txtFinal;
+        };
+    }
+
     public static void doTry() {
-        System.out.println("Functions...");
+        Function<String, String> log = Functions
+            .loggerBuilder("[prx]", "==>");
+
+        log.apply("Imprimir...");
+
+        Functions
+            .loggerBuilder()
+            .apply("Imprimir...");
     }
 
 }
